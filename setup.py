01a67d3a41eb3b930e059388a71483b21b89f9a7
benvand <benjaminfvandersteen@gmail.com>
2017-01-17 20:34:26 +0000

    foo

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ test c
 from setuptools import setup, find_packages
 
 
-version = '1'
+version = '1.0.0'
 
 
 setup(
     name='test_c',
-    version='1',
+    version='1.0.0',
     url='https://github.com/benvand/test_c',
     license='MIT',
     author='benvand',
